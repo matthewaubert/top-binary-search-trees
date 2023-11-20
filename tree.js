9b6df9307a58763dff10c3cf0396616ed41c0c3f
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-11-20 09:45:51 -0500

    Build insert func

--- tree.js
+++ tree.js
@@ -2,8 +2,7 @@ import Node from './node.js';
 import mergeSort from './merge-sort.js';
 import prettyPrint from './pretty-print.js';
 
-// create new binary search tree
-// with appropriate functionality on prototype
+// create new binary search tree with appropriate functionality
 export default class Tree {
   constructor(array) {
     this.root = Tree.#buildTree(array); // root node of tree
@@ -37,8 +36,32 @@ export default class Tree {
       Tree.#buildBranch(array, mid + 1, end),
     );
   }
+
+  // insert a new node to tree; input: node data value
+  insert(value) {
+    this.root = Tree.#insertRec(this.root, value);
+  }
+
+  // recursively traverse down tree nodes to insert value at leaf node
+  // input: tree root, value to insert
+  static #insertRec(root, value) {
+    // base case: if tree root is empty, set root to new node containing value
+    if (root === null) return new Node(value);
+
+    // recursive step: if value < root, recursively insert value into left subtree
+    if (value < root.data) root.left = Tree.#insertRec(root.left, value);
+    // recursive step: if value > root, recursively insert value into right subtree
+    else root.right = Tree.#insertRec(root.right, value);
+
+    return root;
+  }
 }
+
+// const emptyTree = new Tree(); // tree.root = null
 
 const arr1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(arr1);
+// prettyPrint(tree.root);
+
+tree.insert(12);
 prettyPrint(tree.root);
